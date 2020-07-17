a231b0d7f4348d249d4445489c121ee79826d5a9
Raif-fl <flanagan.keegan16@gmail.com>
2020-07-17 14:55:26 -0700

    Altered so that a unique and descriptive name occurs for all matrix combinations

--- yaml_fast.py
+++ yaml_fast.py
@@ -16,35 +16,10 @@ for rows in param:
     subprocess.call(str_args)
     extr=(
         ['./bin/extract-fasta.py', '--input-file','simu.yaml',
-        '--fasta-output-file',x])
+        '--fasta-output-file','shm_freq'+str(x)+'indel_l'+str(y)+'.fasta'])
     str_extr=[str(i) for i in extr]
     subprocess.call(str_extr)
 
-
-
-
-
-
-
-
-
-
-
-
-
-#for rows in param:
-    #for cells in rows:
-
-#x=np.zeros((len(param)))
-#y=np.zeros((len(param)))
-#print(x)
-
-#for n in range(0,(len(param)-1)):
-#    x[n]=param[n,0]
-#    y[n]=param[n,1]
-#    print(x)
-#    print(y)
-
-#parse= argparse.Argumentparser(description='run a simulation given a vector of values and output a fasta file',
-#epilog='the values currently ordered as:')
-#parser.add_argument(--shm_freq,-shm,default=0.5)
+#we want to have this set up so that we can choose how many parameters are inputted,
+#so I need something that will allow me to control the args section through
+#passing information to args.parse
